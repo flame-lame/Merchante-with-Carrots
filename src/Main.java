3bb116f053f4e26c1598ea707fc0b787e4778188
w-k <vaclav.kocum@techstack.cz>
2024-09-29 14:24:09 +0200

    Getting things right

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Merchante sellerFirst = new Merchante("Piotr", "03-11-1985", 11, 8.3, "Střemy", "6Z5 3250", 6.3);
+        Merchante sellerSecond = new Merchante("Vivor", "02-12-1982", 22, 12.3, "Solenice", "2B2 7227", 11.3);
+
+        System.out.println();
+        System.out.println("Name: " + sellerFirst.getName() + " # " + "Birth: " + sellerFirst.getBirth() + " # " + "ActiveContracts: " + sellerFirst.getActiveContracts() + " # " + "Balance: " + sellerFirst.getBalance() + " # " + "Adress: " + sellerFirst.getAddress() + " # " + "CarID: " + sellerFirst.getCarID() + " # " + "CarConsumption: " + sellerFirst.getCarConsumption());
+
+        System.out.println();
+        System.out.println("Name: " + sellerSecond.getName() + " # " + "Birth: " + sellerSecond.getBirth() + " # " + "ActiveContracts: " + sellerSecond.getActiveContracts() + " # " + "Balance: " + sellerSecond.getBalance() + " # " + "Adress: " + sellerSecond.getAddress() + " # " + "CarID: " + sellerSecond.getCarID() + " # " + "CarConsumption: " + sellerSecond.getCarConsumption());
     }
 }
--- src/Merchante.java
+++ src/Merchante.java
@@ -1,4 +1,75 @@
-package PACKAGE_NAME;
-
 public class Merchante {
+    private String name;
+    private String birth;
+    private int activeContracts;
+    private double balance; // in a tons
+    private String address;
+    private String carID;
+    private double carConsumption;
+
+    public Merchante(String name, String birth, int activeContracts, double balance, String address, String carID, double carConsumption) {
+        this.name = name;
+        this.birth = birth;
+        this.activeContracts = activeContracts;
+        this.balance = balance;
+        this.address = address;
+        this.carID = carID;
+        this.carConsumption = carConsumption;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public int getActiveContracts() {
+        return activeContracts;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCarID() {
+        return carID;
+    }
+
+    public double getCarConsumption() {
+        return carConsumption;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public void setActiveContracts(int activeContracts) {
+        this.activeContracts = activeContracts;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCarID(String carID) {
+        this.carID = carID;
+    }
+
+    public void setCarConsumption(double carConsumption) {
+        this.carConsumption = carConsumption;
+    }
 }
